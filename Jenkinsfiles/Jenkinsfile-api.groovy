d2c23ba85c584f7d8a776a6cb8c9b0096437edfd
Jack Billings <jack.billings@ibm.com>
2021-02-25 14:17:25 -0600

    first commit

---
+++ Jenkinsfiles/Jenkinsfile-api.groovy
@@ -0,0 +1,201 @@
+node('master') {
+   jdk = tool name: 'JDK8'
+   env.JAVA_HOME = "${jdk}"
+   env.PATH="${env.JAVA_HOME}/bin:${env.PATH}"
+   def zceeHome
+   stage('Checkout Git Code') { // for display purposes
+      // Get some code from a GitHub repository
+      println "project name is catalogTest"
+      git credentialsId: 'git', url: 'https://github.com/Jack-Billings-IBM/catalog.git'
+   }
+
+   stage('Rebuild zOS Connect API') {
+        println "Calling zconbt"
+        def output = sh (returnStdout: true, script: 'pwd')
+        println output
+        sh "${WORKSPACE}/zconbt/bin/zconbt -pd=${WORKSPACE}/catalog -f=${WORKSPACE}/catalog/catalog.aar "
+        println "Called zconbt for catalog"
+        println "Exiting Stage 2, entering Stage 3!"
+   }
+   stage('Check for and Handle Existing API') {
+       println "Going to stop and remove existing API from zOS Connect Server if required"
+       def resp = stopAndDeleteRunningAPI("catalog")
+       println "Cleared the field for API deploy: "+resp
+   }
+
+    stage('Deploy API to z/OS Connect Server'){
+       //call code to deploy the API.  passing the name of the API as a param
+       def apiFileName ="catalog.aar"
+       installAPI(apiFileName)
+    }
+   
+    stage('Test API') {
+       def serviceName = "inquireSingle"
+       testAPI(serviceName)
+       def serviceName2 = "inquireCatalog"
+       testAPI(serviceName2)
+    }
+
+    stage("Publish Artifacts to Artifactory") {
+       // Obtain an Artifactory server instance, defined in Jenkins --> Manage:
+       def server = Artifactory.server "artifactory"
+
+       // Read the upload spec which was downloaded from github.
+       def uploadSpec = readFile 'Artifactory/apis-upload.json'
+       // Upload to Artifactory.
+       def buildInfo = server.upload spec: uploadSpec
+
+       // Publish the build to Artifactory
+       server.publishBuildInfo buildInfo
+
+      
+    }   
+   
+    stage("Push to GitHub") {
+       sh "rm response.json"
+       sh "rm responseDel.json"
+       sh "rm responseStop.json"
+       sh "git config --global user.email 'jack.billings@ibm.com'"
+       sh "git config --global user.name 'Jack-Billings-IBM'"
+       sh "git add -A"
+       sh "git commit -m 'new aar file'"
+       //need to add git credentials to jenkins
+       withCredentials([usernamePassword(credentialsId: 'git', usernameVariable: 'GIT_USERNAME', passwordVariable: 'GIT_PASSWORD')]){    
+           sh('''
+               git config --local credential.helper "!f() { echo username=\\$GIT_USERNAME; echo password=\\$GIT_PASSWORD; }; f"
+               git push origin HEAD:master
+           ''')
+       }
+    }
+}
+// node('nodejs') {
+//    def templateName = 'egui'
+//    stage('Build egui') {
+//            script {
+//                openshift.withCluster() {
+//                    openshift.withProject() {
+//                      def builds = openshift.selector("bc", templateName).related('builds')
+//                      timeout(5) { 
+//                        builds.untilEach(1) {
+//                          return (it.object().status.phase == "Complete")
+//                        }
+//                      }
+//                    }
+//                }
+//          }
+//        }
+//     stage('Deploy egui') {
+//        script {
+//             openshift.withCluster() {
+//                 openshift.withProject() {
+//                   def rm = openshift.selector("dc", templateName).rollout().latest()
+//                   timeout(5) { 
+//                     openshift.selector("dc", templateName).related('pods').untilEach(1) {
+//                       return (it.object().status.phase == "Running")
+//                     }
+//                   }
+//                 }
+//         }
+//       }
+//     }
+   
+// }
+ 
+
+
+
+   //Will stop a running API if required and delete it
+   def stopAndDeleteRunningAPI(apiName){
+
+       println("Checking existence/status of API: "+apiName)
+
+       //will be building curl commands, so saving the tail end for appending
+       def urlval = "150.238.240.73:31158/zosConnect/apis/"+apiName
+       def stopurlval = "150.238.240.73:31158/zosConnect/apis/"+apiName+"?status=stopped"
+
+       //complete curl command will be saved in these values
+       def command_val = ""
+       def stop_command_val = ""
+       def del_command_val = ""
+
+       //call utility to get saved credentials and build curl command with it.  Commands were built to check, stop and delete API
+       //curl command spits out response code into stdout.  that's then held in response field to evaluate
+       command_val = "curl -o response.json -w %{response_code} --header 'Content-Type:application/json' --insecure "+urlval
+       stop_command_val = "curl -X PUT -o responseStop.json --header \'Accept: application/json\' --header 'Content-Type:application/json' --insecure "+stopurlval
+       del_command_val = "curl -X DELETE -o responseDel.json --header 'Content-Type:application/json' --insecure "+urlval
+      
+       // this checks the initial status of the API.  If it exists, HTTP Response Code will be 200
+       def response = sh (script: command_val, returnStdout: true)
+       println ""
+       println "Response code is: "+response
+
+       if(response != "200"){
+          println "This API does not exist on the server.  Deploying for first Time"
+          return true
+       }
+       else{
+           println "API already exists, stopping and deleting it now"
+           //reading status of existing API from response file.  file was created during curl command.
+           def myObject = readJSON file: 'response.json'
+           println myObject
+           def status = myObject.status
+           println "API status is "+status
+           if(status == "Started"){
+               //Stop API
+               def responseStop = sh (script: stop_command_val, returnStdout: true)
+               def myObjectStop = readJSON file: 'responseStop.json'
+               def statusStop = myObjectStop.status
+              //ensure that status was actually stopped
+               println "New status of API : "+statusStop
+
+              //Delete API using curl command that was built
+               def responseDel = sh (script: del_command_val, returnStdout: true)
+               println "API delete call complete"
+               return true
+           }
+           else{
+               println "Don't know why we hit this! Service is currently stopped......handle this if you like"
+               return false
+           }    
+         return true
+       }
+   }
+
+   def installAPI(apiFileName){
+       println "Starting API deployment now"
+
+       def urlval = "150.238.240.73:31158/zosConnect/apis/"
+       def respCode = ""
+
+      //call utility to get saved credentials and build curl command with it and sar file name and then execute command
+      //curl command spits out response code into stdout.  that's then held in respCode field to evaluate
+       def command_val = "curl -X POST -o response.json -w %{response_code} --header 'Content-Type:application/zip' --data-binary @${WORKSPACE}/catalog/"+apiFileName+" --insecure "+urlval
+       respCode = sh (script: command_val, returnStdout: true)
+
+       println "Service Installation Response code is: "+respCode
+       if(respCode == "201"){
+           println "Deployment completed successfully"
+       }else if(respCode == "409"){
+           error("Deployment failed due to it already existing")
+       }
+   }
+   
+   def testAPI(serviceName) {
+      println "Starting testing now"
+
+      def urlval = "150.238.240.73:31158/catalogManager/items"
+      def respCode = ""
+      
+      //def single = readJSON file: 'tests/inquireSingle_service_request.json'
+      if(serviceName == "inquireSingle") {
+         def command_val = "curl -X GET --fail -w %{response_code} --header 'Accept: application/json' '"+urlval+"/30' -o ${WORKSPACE}/tests/"+serviceName+"_api.json"
+         respCode = sh (script: command_val, returnStdout: true)
+         println serviceName+" API Test Response code is: "+respCode
+      }
+      else {
+         def command_val = "curl -X GET --fail -w %{response_code} --header 'Accept: application/json' '"+urlval+"?startItemID=40' -o ${WORKSPACE}/tests/"+serviceName+"_api.json"
+         respCode = sh (script: command_val, returnStdout: true)
+         println serviceName+" API Test Response code is: "+respCode
+      }
+   }
+
---
+++ Jenkinsfiles/Jenkinsfile-services.groovy
@@ -0,0 +1,175 @@
+
+
+node('master') {
+   jdk = tool name: 'JDK8'
+   env.JAVA_HOME = "${jdk}"
+   env.PATH="${env.JAVA_HOME}/bin:${env.PATH}"
+   
+   stage('Checkout Git Code to Jenkins on OpenShift') { // for display purposes
+      // Get some code from a GitHub repository
+      println "project name is catalog"
+      git credentialsId: 'git', url: 'https://github.com/Jack-Billings-IBM/catalog.git'
+   }
+
+   stage('Rebuild zOS Connect Services') {
+        println "Calling zconbt"
+        def output = sh (returnStdout: true, script: 'pwd')
+        println output
+        sh "${WORKSPACE}/zconbt/bin/zconbt --properties=${WORKSPACE}/properties/inquireCatalog.properties --file=${WORKSPACE}/archives/inquireCatalog.sar "
+        println "Called zconbt for inquireCatalog"
+        sh "${WORKSPACE}/zconbt/bin/zconbt --properties=${WORKSPACE}/properties/inquireSingle.properties --file=${WORKSPACE}/archives/inquireSingle.sar "
+        println "Called zconbt for inquireSingle"
+        println "Exiting Stage 2, entering Stage 3!"
+   }
+   stage('Check for and Handle Existing Services') {
+       println "Going to stop and remove existing service from zOS Connect Server if required"
+       def resp = stopAndDeleteRunningService("inquireSingle")
+       def resp2 = stopAndDeleteRunningService("inquireCatalog")
+       println "Cleared the field for service deploy: "+resp
+       println "Cleared the field for service deploy: "+resp2
+       }
+
+    stage('Deploy to z/OS Connect Server'){
+       //call code to deploy the service.  passing the name of the service as a param
+       def sarFileName ="inquireSingle.sar"
+       installSar(sarFileName)
+       def sarFileName2 ="inquireCatalog.sar"
+       installSar(sarFileName2)
+    }
+   
+    stage('Test Services') {
+       def serviceName = "inquireCatalog"
+       testServices(serviceName)
+       def serviceName2 = "inquireSingle"
+       testServices(serviceName2)
+    }
+   
+    stage("Publish Artifacts to Artifactory") {
+       // Obtain an Artifactory server instance, defined in Jenkins --> Manage:
+       def server = Artifactory.server "artifactory"
+
+       // Read the upload spec which was downloaded from github.
+       def uploadSpec = readFile 'Artifactory/services-upload.json'
+       // Upload to Artifactory.
+       def buildInfo = server.upload spec: uploadSpec
+
+       // Publish the build to Artifactory
+       server.publishBuildInfo buildInfo
+
+      
+    }
+   
+    stage("Push to GitHub") {
+       sh "rm response.json"
+       sh "rm responseDel.json"
+       sh "rm responseStop.json"
+       sh "git config --global user.email 'jack.billings@ibm.com'"
+       sh "git config --global user.name 'Jack-Billings-IBM'"
+       sh "git add -A"
+       sh "git commit -m 'new sar file'"
+       //need to add git credentials to jenkins
+       withCredentials([usernamePassword(credentialsId: 'git', usernameVariable: 'GIT_USERNAME', passwordVariable: 'GIT_PASSWORD')]){    
+           sh('''
+               git config --local credential.helper "!f() { echo username=\\$GIT_USERNAME; echo password=\\$GIT_PASSWORD; }; f"
+               git push origin HEAD:master
+           ''')
+       }
+    }
+}
+
+
+   //Will stop a running service if required and delete it
+   def stopAndDeleteRunningService(service_name){
+
+       println("Checking existence/status of Service name: "+service_name)
+
+       //will be building curl commands, so saving the tail end for appending
+       def urlval = "150.238.240.74:31158/zosConnect/services/"+service_name
+       def stopurlval = "150.238.240.74:31158/zosConnect/services/"+service_name+"?status=stopped"
+
+       //complete curl command will be saved in these values
+       def command_val = ""
+       def stop_command_val = ""
+       def del_command_val = ""
+
+       //call utility to get saved credentials and build curl command with it.  Commands were built to check, stop and delete service
+       //curl command spits out response code into stdout.  that's then held in response field to evaluate
+       command_val = "curl -o response.json -w %{response_code} --header 'Content-Type:application/json' --insecure "+urlval
+       stop_command_val = "curl -X PUT -o responseStop.json --header \'Accept: application/json\' --header 'Content-Type:application/json' --insecure "+stopurlval
+       del_command_val = "curl -X DELETE -o responseDel.json --header 'Content-Type:application/json' --insecure "+urlval
+      
+       // this checks the initial status of the service.  If it exists, HTTP Response Code will be 200
+       def response = sh (script: command_val, returnStdout: true)
+       println ""
+       println "Response code is: "+response
+
+       if(response != "200"){
+          println "This Service does not exist on the server.  Deploying for first Time"
+          return true
+       }
+       else{
+           println "Service already exists, stopping and deleting it now"
+           //reading status of existing service from response file.  file was created during curl command.
+           def myObject = readJSON file: 'response.json'
+           def status = myObject.zosConnect.serviceStatus
+           println "Service status is "+status
+           if(status == "Started"){
+               //Stop API
+               def responseStop = sh (script: stop_command_val, returnStdout: true)
+               def myObjectStop = readJSON file: 'responseStop.json'
+               def statusStop = myObjectStop.zosConnect.serviceStatus
+              //ensure that status was actually stopped
+               println "New status of service : "+statusStop
+
+              //Delete API using curl command that was built
+               def responseDel = sh (script: del_command_val, returnStdout: true)
+               println "Service delete call complete"
+               return true
+           }
+           else{
+               println "Don't know why we hit this! Service is currently stopped......handle this if you like"
+               return false
+           }    
+         return true
+       }
+   }
+
+   def installSar(sarFileName){
+       println "Starting sar deployment now"
+
+       def urlval = "150.238.240.74:31158/zosConnect/services/"
+       def respCode = ""
+
+      //call utility to get saved credentials and build curl command with it and sar file name and then execute command
+      //curl command spits out response code into stdout.  that's then held in respCode field to evaluate
+       def command_val = "curl -X POST -o response.json -w %{response_code} --header 'Content-Type:application/zip' --data-binary @${WORKSPACE}/archives/"+sarFileName+" --insecure "+urlval
+       respCode = sh (script: command_val, returnStdout: true)
+
+       println "Service Installation Response code is: "+respCode
+       if(respCode == "201"){
+           println "Deployment completed successfully"
+       }else if(respCode == "409"){
+           error("Deployment failed due to it already existing")
+       }
+   }
+   
+   def testServices(serviceName) {
+      println "Starting testing now"
+
+      def urlval = "150.238.240.74:31158/zosConnect/services/"+serviceName+"?action=invoke"
+      def respCode = ""
+      
+      //def single = readJSON file: 'tests/inquireSingle_service_request.json'
+      if(serviceName == "inquireSingle") {
+         def single = '''{"DFH0XCMNOperation":{"ca_single_request_id":"01INQS","ca_inquire_single":{"ca_item_ref_req":40}}}'''
+         def command_val = "curl -g -X POST -o ${WORKSPACE}/tests/"+serviceName+"_service.json -w %{response_code} --header 'Content-Type: application/json' --data "+single+" --insecure "+urlval
+         respCode = sh (script: command_val, returnStdout: true)
+         println serviceName+" Service Test Response code is: "+respCode
+      }
+      else {
+         def catalog = '{"DFH0XCMNOperation":{"ca_request_id":"01INQC","ca_inquire_request":{"ca_list_start_ref":20}}}'
+         def command_val = "curl -g -X POST -o ${WORKSPACE}/tests/"+serviceName+"_service.json -w %{response_code} --header 'Content-Type: application/json' --header 'Content-Type: text/plain' --data "+catalog+" --insecure "+urlval
+         respCode = sh (script: command_val, returnStdout: true)
+         println serviceName+" Service Test Response code is: "+respCode
+      }
+   }
