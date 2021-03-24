f0983e13b46eff15943904992c6d3f267cf266ba
Jack-Billings-IBM <44097415+Jack-Billings-IBM@users.noreply.github.com>
2021-03-24 15:26:48 -0500

    Update Jenkinsfile-build-services.groovy

--- Jenkinsfiles/Jenkinsfile-build-services.groovy
+++ Jenkinsfiles/Jenkinsfile-build-services.groovy
@@ -52,4 +52,21 @@ node('master') {
         }         
     }
    
+   stage("test reading file names") {
+      def listfiles(dir) {
+         dlist = []
+         flist = []
+         new File(dir).eachDir {dlist << it.name }
+         dlist.sort()
+         new File(dir).eachFile(FileType.FILES, {flist << it.name })
+         flist.sort()
+         return (dlist << flist).flatten()
+      }
+
+      fs = listfiles(".")
+      fs.each {
+         println it
+      }
+   }
+   
 }
