222899248e94b0f9ba6a8fa42ecb4498e68609b8
Jack-Billings-IBM <44097415+Jack-Billings-IBM@users.noreply.github.com>
2021-03-25 15:12:36 -0500

    Create Jenkinsfile-build-select-apis.groovy

---
+++ Jenkinsfiles/Jenkinsfile-build-select-apis.groovy
@@ -0,0 +1,66 @@
+node('master') {
+   jdk = tool name: 'JDK8'
+   env.JAVA_HOME = "${jdk}"
+   env.PATH="${env.JAVA_HOME}/bin:${env.PATH}"
+   
+   def apis = [ "${service1}", "${service2}"]
+   stringNum = "${number_of_apis}"
+   int intNum = stringNum as int  
+   
+   stage('Checkout Git Code to Jenkins on OpenShift') { // for display purposes
+      // Get some code from a GitHub repository
+      // need to define git credentials
+      git credentialsId: 'git', url: '${git_url}'
+   }
+
+
+   stage('Build zOS Connect APIs') {
+           println "The number of APIs to be built: " + intNum
+           println "Building these APIs: "+apis
+           
+           //create sar file for each api
+           for (int i = 0; i < intNum; i++) {
+              println "Building API "+apis[i]
+              sh "${WORKSPACE}/zconbt/bin/zconbt -pd=./"+apis[i]+" -f=./"+apis[i]+".aar" 
+           }
+           println "aar files that have been built: "
+           println "${apis}"
+           sh "rm *.aar"
+           println "Exiting Stage 2, entering Stage 3!"
+        }
+   }
+
+   
+    stage("Publish Artifacts to Artifactory") {
+       // Obtain an Artifactory server instance, defined in Jenkins --> Manage:
+       // need to define artifactory server in Jenkins
+       def artifactory_server = Artifactory.server "artifactory"
+       println "Publishing to server: "+artifactory_server
+       
+       int intNum = apis.size()
+       println "The number of APIs to publish: " + intNum
+       
+       // loops thorugh each aar created and publishes it to your artifactory server
+       for (int i = 0; i < intNum; i++) {
+          def aarFileName = apis[i] 
+          println "Publishing aar file: "+aarFileName
+          def uploadSpec = """{
+            "files": [
+               {
+                  "pattern": "${aarFileName}.aar",
+                  "target": "${artifactory_repo_name}/apis/"
+               }
+               ]
+            }"""
+            println "Uploading"
+            // Upload to Artifactory.
+            def buildInfo = artifactory_server.upload spec: uploadSpec
+
+            // Publish the build to Artifactory
+            artifactory_server.publishBuildInfo buildInfo
+          
+            println "Upload successful"
+        }         
+    }
+   
+}
